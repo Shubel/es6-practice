4d5dcd21918024d54f823145c421edf6d354dde2
Sunny <shubelislam.diu@gmail.com>
2021-05-04 18:28:04 +0600

    template.js done

---
+++ template.js
@@ -0,0 +1,17 @@
+const firstName = 'Shubel';
+const lastName = 'Islam';
+// const totalName = firstName + ' ' + lastName;
+const totalName = `${firstName} ${lastName} is a good boy.....`;
+console.log(totalName);
+
+const multiLine = 'He is a good boy\n'
+                + 'He is a good student\n'
+                + 'He works very well......';
+console.log(multiLine);
+
+const multiLine2 = `Mofiz loves you
+Mofiz needs you
+no. She dont like him
+She is rough.....`;
+console.log(multiLine2);
+
