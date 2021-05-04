a304b190d38c40dd12b38817162fb2eb5645d0c0
Sunny <shubelislam.diu@gmail.com>
2021-05-04 21:40:57 +0600

    arrow function in es6

---
+++ arrow.js
@@ -0,0 +1,15 @@
+// function doubleIt(num){
+//     return num * 2;
+// }
+
+// const doubleIt = function(num){
+//     return num * 2;
+// }
+
+const doubleIt = num => num * 2;
+const total = doubleIt(50);
+console.log(total);
+
+const add = (num1, num2) => num1 + num2;
+const result = add(20, 30);
+console.log(result);
