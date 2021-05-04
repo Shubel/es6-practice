e35031ebf16e4dce6b1890881e11adc0dc444a06
Sunny <shubelislam.diu@gmail.com>
2021-05-04 13:40:27 +0600

    letconst.js uploded

---
+++ letconst.js
@@ -0,0 +1,22 @@
+// const variable 
+const myName = 'sunny';
+console.log(myName);
+
+const numbers = [12, 43];
+numbers[1] = 50;
+numbers.push(68);
+console.log(numbers);
+
+const superStar = {names : 'sunny khan', phoneNum : 76754};
+console.log(superStar);
+
+// let variable
+let patientName = 'Rahim Khala';
+patientName = 'Fatema Khala';
+console.log(patientName);
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    sum = sum + i;
+    console.log(sum);
+}
