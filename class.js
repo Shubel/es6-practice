51592e07dd283acf0d737e31243db8b06810c3c6
Sunny <shubelislam.diu@gmail.com>
2021-05-05 10:58:09 +0600

    create object from class

---
+++ class.js
@@ -0,0 +1,10 @@
+class Student{
+    constructor(sID, sName){
+        this.id = sID;
+        this.name = sName;
+        this.school = "kader Molla high School";
+    }
+}
+const student1 = new Student(18, "Kolim Uddin");
+const student2 = new Student(22, "Kuddus Boyati");
+console.log(student1, student2);
